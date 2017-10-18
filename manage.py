5ab41bfec1cff724910bcb26579d79a6aa1c433f
colonelrascals <pdhildreth@gmail.com>
2017-10-18 15:15:00 -0500

    database added and dockerized

--- manage.py
+++ manage.py
@@ -1,9 +1,15 @@
 from flask_script import Manager
 
-from project import app
+from project import app, db
 # created a new Manager instance to handle all of the manager commands from the command line.
 manager = Manager(app)
 
+@manager.command
+def recreate_db():
+    """Recreates a database."""
+    db.drop_all()
+    db.create_all()
+    db.session.commit()
 
 if __name__ == '__main__':
     manager.run()

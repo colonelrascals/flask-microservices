92df830bc250003628c2ed31294bddb08fb56878
colonelrascals <pdhildreth@gmail.com>
2017-10-18 15:23:34 -0500

    tests

--- manage.py
+++ manage.py
@@ -1,6 +1,8 @@
 from flask_script import Manager
 
 from project import app, db
+
+import unittest
 # created a new Manager instance to handle all of the manager commands from the command line.
 manager = Manager(app)
 
@@ -10,6 +12,15 @@ def recreate_db():
     db.drop_all()
     db.create_all()
     db.session.commit()
+
+@manager.command
+def test():
+    """Runs the tests without code coverage."""
+    tests = unittest.TestLoader().discover('project/tests', pattern='test*.py')
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+    if result.wasSuccessful():
+        return 0
+    return 1
 
 if __name__ == '__main__':
     manager.run()
